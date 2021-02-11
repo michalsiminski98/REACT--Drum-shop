c6eeefdcbe0626f97575dc83de2f294d7665a9d0
Michal Siminski <michal.siminski3@gmail.com>
2021-02-11 20:55:30 +0100

    add error page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import ShopPage from '../pages/ShopPage';
+import ContactPage from '../pages/ContactPage';
+import CartPage from '../pages/CartPage';
+import ErrorPage from '../pages/ErrorPage';
 
 const Page = () => {
     return ( 
-        <div>
-            dziala
-        </div>
+        <Switch>
+            <Route path="/" exact component={ShopPage}/>
+            <Route path="/contact" component={ContactPage}/>
+            <Route path="/cart" component={CartPage}/>
+            <Route component={ErrorPage}/>
+        </Switch>
      );
 }
  
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return ( 
+        <div>
+            kontakcik
+        </div>
+     );
+}
+ 
+export default ContactPage;

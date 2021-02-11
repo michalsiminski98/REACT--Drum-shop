c0140366aae2404da71a02d9f59c131dfc87f4b8
Michal Siminski <michal.siminski3@gmail.com>
2021-02-11 20:15:11 +0100

    add navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
+import '../styles/Navigation.css';
+
+const navigationList = [
+    {name: "Shop", path:'/'},
+    {name: "Contact", path:'/contact'},
+    {name: "cart", path:'/cart'},
+]
+
 const Navigation = () => {
+
+    const menu = navigationList.map(item => (
+        <li className="navigation__li">
+            <NavLink to={item.path}>{item.name === 'cart' ? <i class="fas fa-shopping-cart"></i> : item.name}</NavLink>
+        </li>
+    ))
+
     return ( 
-        <div>
-            dziala
-        </div>
+        <nav className="navigationWrapper">
+            <ul className="navigation">
+                {menu}
+            </ul>
+        </nav>
      );
 }
  

6778879183f874546b4acaee07bd899251329afb
LAPTOP-BO272NV7\simin <michal.siminski3@gmail.com>
2021-02-14 16:32:27 +0100

    prepare product site

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ProductPage extends Component{
+
+
+    render(){
+        return(
+
+            <div>
+                asd
+            </div>
+
+
+        )
+    }
+}
+
+export default ProductPage;
--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+const items = [
+    {id: 1, name: 'drum', price:52},
+    {id: 2, name: 'crash', price:20},
+    {id: 3, name: 'hi-hat', price:100},
+    {id: 4, name: 'tom', price:90},
+    {id: 5, name: 'tom-12', price:5},
+]
+
 const ShopPage = () => {
-    return ( 
+    
+    const menu = items.map(item => {
         <div>
-            Sklep
+            asd
         </div>
+    })
+    
+    return ( 
+        <section className="shopWrapper">
+            {menu}
+        </section>
      );
 }
  
